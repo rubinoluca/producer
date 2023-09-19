d3bd94bfcf0996aa276b8d0a703b1236c51dc170
Luru99 <lu.rubino@reply.it>
2023-09-19 12:35:00 +0200

    up

--- src/main/java/com/codecraftshop/openshift/mysql/controller/StudentController.java
+++ src/main/java/com/codecraftshop/openshift/mysql/controller/StudentController.java
@@ -22,6 +22,6 @@ public class StudentController {
 	
 	@RequestMapping(value="/hello")
     	public String sendHello() {
-        	return "Hello da test!";
+        	return "Hello da test! :)";
     	}
 }
